4794ee39dcaf9612fa8fdc85c1aeeb0096e194af
Carlos Roberto <carlosrjhoe@gmail.com>
2021-08-26 21:22:33 -0300

    adiciona soma

--- calculadora.py
+++ calculadora.py
@@ -2,7 +2,9 @@
 # A partir de então a equipe se alinha qual função cada membro vai implementar
 # Cada pessoa criará um branch com "feature/nome-da-operacao" e irá implementar a respectiva funcao
 # Depois cada membro irá abrir um Pull Request para o branch develop 
-
+def soma(numero_a, numero_b):
+    return numero_a + numero_b
+    
 numero_a = int(input("Digite o primeiro numero: "))
 operacao = input("Digite a operacao (+, -, * ou /): ")
 numero_b = int(input("Digite o segundo numero: "))
